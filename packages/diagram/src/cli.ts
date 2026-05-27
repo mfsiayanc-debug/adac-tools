f27eb82f93fcab7d6c98ef1f068e9318b48b3f27
mfsiayanc-debug <mfsi.ayanc@gmail.com>
2026-05-27 15:18:57 +0530

    feat: lint formatting added

--- packages/diagram/src/cli.ts
+++ packages/diagram/src/cli.ts
@@ -30,7 +30,7 @@ runCLI({
   generateDiagram: async (
     input: string,
     output: string,
-    layoutOverride?: 'elk' | 'dagre' | 'custom',
+    layoutOverride?: 'elk' | 'dagre',
     validate?: boolean,
     _costData?: Record<string, number>,
     period?: string,
@@ -46,7 +46,7 @@ runCLI({
     return generateDiagram(
       input,
       output,
-      layoutOverride,
+      layoutOverride as any,
       validate,
       perServiceCosts,
       period as CostPeriod,
